ca971a0092cc55b8f20d6d9343b5c801322e7324
suzukiryoji <suzukiryoji@localhost>
2021-04-09 18:18:22 +0900

    Write codes

--- src/code9/Main.java
+++ src/code9/Main.java
@@ -2,9 +2,13 @@ package code9;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h;
-		h = new Hero();
-		h.hp = 100;
+		Hero h1;
+		h1 = new Hero();
+		h1.hp = 100;
+		Hero h2;
+		h2 = h1;
+		h2.hp = 200;
+		System.out.println(h1.hp);
 	}
 
 }
---
+++ src/code9/Sword.java
@@ -0,0 +1,7 @@
+package code9;
+
+public class Sword {
+	String name;
+	int damage;
+
+}
